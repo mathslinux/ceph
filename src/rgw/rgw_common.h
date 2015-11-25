03d768617a4624c67d9da985b269babf0e5441d9
clever215 <wu.kongming@h3c.com>
2015-11-25 11:31:48 -0500

    rgw: add an inspection to the field of type when assigning user caps
    Bug #13096
    
    This modification adds the check to the field of type of a user's capality while previous versions set it any value. i.e. we limit the option of types to the 9 certain values, which are "users|buckets|metadata|usage|zone|bilog|mdlog|datalog|ops These 9 choosens are found in ceph documents and in source codes.
    
    Signed-off-by: Kongming Wu <wu.kongming@h3c.com>
    (cherry picked from commit 13a12a5364dc4fa0623bfd543c081a616b32682d)
    Signed-off-by: Dunrong Huang <riegamaths@gmail.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -414,6 +414,7 @@ public:
      DECODE_FINISH(bl);
   }
   int check_cap(const string& cap, uint32_t perm);
+  bool is_valid_cap_type(const string& tp);
   void dump(Formatter *f) const;
   void dump(Formatter *f, const char *name) const;
 
