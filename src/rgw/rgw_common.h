bb319b6445a3da35052523a2ac182b47edd2ca6c
Dunrong Huang <riegamaths@gmail.com>
2015-12-23 21:42:28 +0800

    rgw: add a new error code for email conflict
    From http://docs.ceph.com/docs/master/radosgw/adminops/ when provided email
    address exists, rgw should return EmailExists rather than UserAlreadyExists
    
    Signed-off-by: Dunrong Huang <riegamaths@gmail.com>
    (cherry picked from commit c22731698cab7962f3c62934a37d422973ef157f)
    Signed-off-by: Dunrong Huang <riegamaths@gmail.com>
    
    Conflicts:
    	src/rgw/rgw_common.h
    	src/rgw/rgw_user.cc

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -139,6 +139,7 @@ using ceph::crypto::MD5;
 #define ERR_INVALID_ACCESS_KEY   2028
 #define ERR_MALFORMED_XML        2029
 #define ERR_USER_EXIST           2030
+#define ERR_EMAIL_EXIST          2032
 #define ERR_USER_SUSPENDED       2100
 #define ERR_INTERNAL_ERROR       2200
 
